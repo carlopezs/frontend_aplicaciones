83633ed7da4b976677dacb6e9f974c301abc2c97
Carlos Lopez <carlos@nousproyect.com>
2021-06-03 17:55:11 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import { BrowserRouter as Router,
+
+} from 'react-router-dom';
+import Header from './header';
+
+function App() {
+  return (
+    <div className="App">
+      <Router></Router>
+      <Header>
+      </Header>
+    </div>
+  );
+}
+
+export default App;
