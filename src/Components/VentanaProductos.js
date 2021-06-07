6ac34f891f5111bb782b23f5a0a9d0de3d348019
JohnG6667 <60703340+JohnG6667@users.noreply.github.com>
2021-06-07 16:44:03 -0500

    Insertar Productos

--- src/Components/VentanaProductos.js
+++ src/Components/VentanaProductos.js
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react';
-import { Dialog, DialogTitle, DialogContent} from '@material-ui/core';
+import { Dialog, DialogTitle, DialogContent, Button} from '@material-ui/core';
 import {ProductAjusCard} from './ProductAjusCard';
 import { getProductsAjuste } from "../helpers/Products";
-
+import {VentanaInsertar} from './Insert.js';
+import { Box } from "@material-ui/core";
 
 
 /* export default function VentanaProductos(props){
@@ -22,9 +23,13 @@ import { getProductsAjuste } from "../helpers/Products";
 
 
 
-export default function VentanaProductos(props) {
+export default function VentanaProductos(props, product) {
     const { onClose, selectedValue, open, setDetalleProductos} = props;
-  
+    const [modal, setModal] = useState(false);
+
+    const abrirCerrarModal = () => {
+      setModal((modal) => !modal);
+    };
     const handleClose = () => {
       onClose(selectedValue);
     };
@@ -44,12 +49,33 @@ export default function VentanaProductos(props) {
         <Dialog onClose={handleClose} open={open}>
         <DialogTitle>
             <div>Productos</div>
+            <Box
+            display="flex"
+            flexWrap="wrap"
+            justifyContent="center"
+          >
+            <Button
+              onClick={abrirCerrarModal}
+              size="large"
+              variant="contained"
+              color="primary"
+            >
+              Insertar
+            </Button>
+          </Box>
         </DialogTitle>
+        
+        
         <DialogContent>
              
              {products.map(res => (<ProductAjusCard key={res.pro_id} product={res} setDetalleProductos={setDetalleProductos} setProducts={setProducts}></ProductAjusCard>))}
            
         </DialogContent>
+        <VentanaInsertar
+            setProducts={setProducts}
+            modal={modal}
+            setModal={setModal}
+            insert={false}></VentanaInsertar>
     </Dialog>
     );
   }
--- src/helpers/Products.js
+++ src/helpers/Products.js
@@ -19,3 +19,14 @@ export const updateProducts = async (pro_id,pro_nombre,pro_descripcion,pro_iva,p
   const data = await response.json();
   return data;
 };
+
+export const insertProduct = async (pro_nombre, pro_descripcion, pro_iva, pro_costo, pro_pvp, pro_activo, pro_stock) => {
+  const url = `https://proyecto-nosotros.herokuapp.com/productos/?pro_nombre=${encodeURI(pro_nombre)}
+                &pro_descripcion=${encodeURI(pro_descripcion)}&pro_iva=${pro_iva}
+                &pro_costo=${pro_costo}&pro_pvp=${pro_pvp}&pro_activo=${pro_activo}
+                &pro_stock=${pro_stock}`;
+  const response = await fetch(url, { method: "POST" });
+  const data = await response.json();
+  console.log(data)
+  return data;
+};
