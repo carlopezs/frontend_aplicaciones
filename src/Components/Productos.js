dc1e5f1753af264d04cc05aaf009506b87868bec
Carlos Lopez <carlos@nousproyect.com>
2021-06-07 20:54:21 -0500

    fix:merge

--- src/Components/Productos.js
+++ src/Components/Productos.js
@@ -3,34 +3,58 @@ import { getProducts } from "../helpers/Products";
 import { useEffect, useState } from "react";
 import { ProductCard } from "./ProductCard";
 import { Box } from "@material-ui/core";
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles } from "@material-ui/core/styles";
 import { SearchProducts } from "./SearchProducts";
+import noSearch from "../assets/noSearch.png";
 
-  
 const useStyles = makeStyles(() => ({
-  
-    content: {
-      padding: 10,
-    },
-  }));
-
+  content: {
+    padding: 10,
+  },
+}));
 
 export const Productos = () => {
-    const boxStyle = useStyles(); 
-   const [products, setProducts] = useState({data:[],loading:false});
+  const boxStyle = useStyles();
+  const [products, setProducts] = useState({ data: [], loading: false });
 
-   useEffect(() => {
-       getProducts().then((arrayProducts) =>{
-           setProducts({data:arrayProducts,loading:true});
-       })
-   },[])
+  useEffect(() => {
+    getProducts().then((arrayProducts) => {
+      setProducts({ data: arrayProducts, loading: true });
+    });
+  }, []);
+
+  console.log(products);
 
   return (
     <>
-    {products.loading &&<SearchProducts setProducts={setProducts} products={products}></SearchProducts>}
-    <Box className={boxStyle.content} display="flex" flexWrap="wrap" justifyContent="center"  > 
-      {products.data.map(res => (<ProductCard key={res.pro_id} product={res} setProducts={setProducts}></ProductCard>))}
-    </Box>
+      {products.loading && (
+        <SearchProducts
+          setProducts={setProducts}
+          products={products}
+        ></SearchProducts>
+      )}
+      {products.data.length !== 0 && (
+        <Box
+          className={boxStyle.content}
+          display="flex"
+          flexWrap="wrap"
+          justifyContent="center"
+        >
+          {products.data.map((res) => (
+            <ProductCard
+              key={res.pro_id}
+              product={res}
+              setProducts={setProducts}
+            ></ProductCard>
+          ))}
+        </Box>
+      )}
+      {products.data.length === 0 && (
+        <Box display="flex" justifyContent="center" alignItems="center" flexDirection="column" >
+          <img style={{ width: "430px" }} src={noSearch} />
+          <h2>No se encuentran productos</h2>
+        </Box>
+      )}
     </>
   );
 };
